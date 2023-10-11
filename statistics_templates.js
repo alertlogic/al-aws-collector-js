a02ff3c02b7fc34c1a8a888f36f0345939e36f45
imranalisyed506 <105209301+imranalisyed506@users.noreply.github.com>
2023-10-11 18:00:15 +0530

    initial test case

--- statistics_templates.js
+++ statistics_templates.js
@@ -9,96 +9,53 @@
  * -----------------------------------------------------------------------------
  */
 
-const AWS = require('aws-sdk');
-const async = require('async');
 const moment = require('moment');
 
-const m_alAws = require('./al_aws');
-
-const AWS_STATISTICS_PERIOD_MINUTES = 15;
+// const { getMetricStatistics } = require('./awssdkv3_utils');
 const MAX_ERROR_MSG_LEN = 1024;
 
-/**
- * @typedef {Object} Stat
- * @property {string} Label - status of healtcheck (ok, warning, error).
- * @property {array} Datapoints - array of datapoints.
- *
-*/
 
-/**
- * @typedef {Object} StatError
- * @property {string} Label - status of healtcheck (ok, warning, error).
- * @property {string} StatisticsError - description of error happened during getting statistic
- *
-*/
+const AWS_STATISTICS_PERIOD_MINUTES = 15;
+const { CloudWatchClient, GetMetricStatisticsCommand } = require("@aws-sdk/client-cloudwatch");
 
+var getMetricStatistics = async function (params, callback) {
+    var cloudwatch = new CloudWatchClient({apiVersion: '2010-08-01'}); 
 
-/**
- * All custom statistic funs should follow the same interface as this function.
- *
- * @function
- *
- * @param {function} callback - callback, which is called by health check when it's done.
- *
- * @returns {function} callback(err, result)
- *
- *     {Error}  err             - The Error object if an error occurred, null otherwise
- *     {Stat|StatError}  result - The Stat object, or StatError if error occured
- *
- */
+    const command = new GetMetricStatisticsCommand(params);
 
-// function customStatFunExample(callback) {
-//     return callback(null, {
-//         Label : 'CustomLabel',
-//         Datapoints : [
-//             {'Timestamp':'2017-11-21T16:40:00Z','Sum':1,'Unit':'Count'}
-//         ]
-//     });
-// }
+    try {
+        const data = await cloudwatch.send(command);
+        return callback(null, {
+            Label: data.Label,
+            Datapoints: data.Datapoints
+        });
 
-var getMetricStatistics = function (params, callback) {
-    var cloudwatch = new AWS.CloudWatch({apiVersion: '2010-08-01'});
-    cloudwatch.getMetricStatistics(params, function(err, data) {
-        if (err) {
-            return callback(null, {
-                Label: params.MetricName,
-                StatisticsError: JSON.stringify(err).slice(0, MAX_ERROR_MSG_LEN)
-            });
-        } else {
-            return callback(null, {
-                Label: data.Label,
-                Datapoints: data.Datapoints
-            });
-        }
-    });
+    } catch (err) {
+        return callback(null, {
+            Label: params.MetricName,
+            StatisticsError: JSON.stringify(err).slice(0, MAX_ERROR_MSG_LEN)
+        });
+    }
 };
 
-var getLambdaMetrics = function (functionName, metricName, callback) {
+var getLambdaMetrics =  function (functionName, metricName, callback) {
     var params = {
         Dimensions: [
-              {
-                  Name: 'FunctionName',
-                  Value: functionName
-              }
+            {
+                Name: 'FunctionName',
+                Value: functionName
+            }
         ],
         MetricName: metricName,
         Namespace: 'AWS/Lambda',
         Statistics: ['Sum'],
         StartTime: moment().subtract(AWS_STATISTICS_PERIOD_MINUTES, 'minutes').toISOString(),
         EndTime: new Date(),
-        Period: 60*AWS_STATISTICS_PERIOD_MINUTES   /* 15 mins as seconds */
+        Period: 60 * AWS_STATISTICS_PERIOD_MINUTES
     };
-    return getMetricStatistics(params, callback);
+    return  getMetricStatistics(params, callback);
 };
-/**
- * 
- * @param {*} functionName - Lambda function name
- * @param {*} metricName -Custom metrics name
- * @param {*} namespace - Custom namespace 
- * @param {*} customDimesions - Extra dimentions object other than function name if you added while creating the custom metrics
- * @param {*} callback 
- * @returns 
- */
+
 var getCustomMetrics = function (functionName, metricName, namespace, customDimesions, callback) {
     let dimensions =
         [
@@ -117,7 +74,7 @@ var getCustomMetrics = function (functionName, metricName, namespace, customDime
         Statistics: ['Sum'],
         StartTime: moment().subtract(AWS_STATISTICS_PERIOD_MINUTES, 'minutes').toISOString(),
         EndTime: new Date(),
-        Period: 60 * AWS_STATISTICS_PERIOD_MINUTES   /* 15 mins as seconds */
+        Period: 60 * AWS_STATISTICS_PERIOD_MINUTES
     };
     return getMetricStatistics(params, callback);
 };
@@ -125,44 +82,43 @@ var getCustomMetrics = function (functionName, metricName, namespace, customDime
 var getKinesisMetrics = function (streamName, metricName, callback) {
     var params = {
         Dimensions: [
-              {
-                  Name: 'StreamName',
-                  Value: streamName
-              }
+            {
+                Name: 'StreamName',
+                Value: streamName
+            }
         ],
         MetricName: metricName,
         Namespace: 'AWS/Kinesis',
         Statistics: ['Sum'],
         StartTime: moment().subtract(AWS_STATISTICS_PERIOD_MINUTES, 'minutes').toISOString(),
         EndTime: new Date(),
-        Period: 60*AWS_STATISTICS_PERIOD_MINUTES   /* 15 mins as seconds */
+        Period: 60 * AWS_STATISTICS_PERIOD_MINUTES
     };
     return getMetricStatistics(params, callback);
 };
 
-var getAllKinesisMetricsFuns = function(streamName) {
+var getAllKinesisMetricsFuns = function (streamName) {
     [
-        function(callback) {
+        function (callback) {
             return getKinesisMetrics(streamName, 'IncomingRecords', callback);
         },
-        function(callback) {
+        function (callback) {
             return getKinesisMetrics(streamName, 'IncomingBytes', callback);
         },
-        function(callback) {
+        function (callback) {
             return getKinesisMetrics(streamName, 'ReadProvisionedThroughputExceeded', callback);
         },
-        function(callback) {
+        function (callback) {
             return getKinesisMetrics(streamName, 'WriteProvisionedThroughputExceeded', callback);
         }
     ]
 }
 
 module.exports = {
-    getMetricStatistics : getMetricStatistics,
-    getLambdaMetrics : getLambdaMetrics,
-    getKinesisMetrics : getKinesisMetrics,
-
+    getMetricStatistics: getMetricStatistics,
+    getLambdaMetrics: getLambdaMetrics,
+    getKinesisMetrics: getKinesisMetrics,
     // functions which return funs:
-    getAllKinesisMetricsFuns : getAllKinesisMetricsFuns,
+    getAllKinesisMetricsFuns: getAllKinesisMetricsFuns,
     getCustomMetrics: getCustomMetrics,
 };
